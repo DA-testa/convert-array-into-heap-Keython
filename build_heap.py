7ac8e3a86bd0b8295455a06ab73b630957eb73bc
Keython <keita.laimina@gmail.com>
2023-03-19 05:05:29 +0200

    sadi?

--- build_heap.py
+++ build_heap.py
@@ -1,12 +1,23 @@
 # python3
 
-
 def build_heap(data):
     swaps = []
     # TODO: Creat heap and heap sort
+    for i in range((len(data))//2,-1,-1):
+        while True:
+            lb=2*i+1
+            kr=2*i+2
+            minim=i
+            if lb<len(data) and data[lb]<data[minim]:
+                minim=lb
+            if kr<len(data) and data[kr]<data[minim]:
+                minim=kr
+            if minim!=i:
+                data[i],data[minim]=data[minim],data[i]
+                swaps.append((i,minim))
+            if minim==i:
+                break
     # try to achieve  O(n) and not O(n2)
-
-
     return swaps
 
 
@@ -15,11 +26,17 @@ def main():
     # TODO : add input and corresponding checks
     # add another input for I or F 
     # first two tests are from keyboard, third test is from a file
-
-
+    text=input("ievadi I vai F:")
+    if 'F' in text:
+        fails=input("faila nosaukums:")
+        with open("./tests/" + fails) as f:  
+                    n = int(f.readline())
+                    data = list(map(int, f.readline().split()))
+                     
     # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
+    elif 'I' in text:
+        n = int(input())
+        data = list(map(int, input().split()))
 
     # checks if lenght of data is the same as the said lenght
     assert len(data) == n
@@ -30,8 +47,6 @@ def main():
 
     # TODO: output how many swaps were made, 
     # this number should be less than 4n (less than 4*len(data))
-
-
     # output all swaps
     print(len(swaps))
     for i, j in swaps:
